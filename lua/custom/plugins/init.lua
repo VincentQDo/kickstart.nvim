15e5fe3bd9e03ab73e8c7698ca17004427bab8af
VincentQDo <vincentquangdo@gmail.com>
2025-11-18 10:24:07 +0000

    Update custom config with more custom keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,18 @@
 
 ---@module 'lazy'
 ---@type LazySpec
+local opt = vim.opt
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.mouse = ''
+
+vim.opt.relativenumber = true
+vim.keymap.set('n', '<leader>rn', function()
+  vim.opt.relativenumber = not vim.opt.relativenumber:get()
+end, { desc = 'Toggle [r]elative [n]umber' })
+
+-- Visual surround with mini.surround using `s`
+vim.keymap.set('v', 's', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true, desc = 'Add surround (visual)' })
 return {}
